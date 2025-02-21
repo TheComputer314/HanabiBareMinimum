c89ef4044efb23521f703a74f1adfb574c3289c5
ハイドラント <thecomputer314@gmail.com>
2025-02-20 23:12:43 -0600

    Convert to Command-based

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,12 +5,15 @@
 
 package frc.robot;
 
+import static edu.wpi.first.wpilibj2.command.Commands.none;
+
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.swerve.Swerve;
 
 /**
@@ -19,94 +22,48 @@ import frc.robot.swerve.Swerve;
  * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
-  private static final String DEFAULT_AUTO = "Default";
-  private static final String CUSTOM_AUTO = "My Auto";
-  private String autoSelected;
-  private final SendableChooser<String> chooser = new SendableChooser<>();
-
-  private final Swerve swerve;
-
-  private final XboxController driverController;
+  private final SendableChooser<Command> chooser = new SendableChooser<>();
 
   /**
    * This method is run when the robot is first started up and should be used for any initialization
    * code.
    */
   public Robot() {
-    chooser.setDefaultOption("Default Auto", DEFAULT_AUTO);
-    chooser.addOption("My Auto", CUSTOM_AUTO);
+    chooser.setDefaultOption("Default Auto", none());
     SmartDashboard.putData("Auto choices", chooser);
 
-    swerve = new Swerve();
-    driverController = new XboxController(0);
+    Swerve swerve = new Swerve();
+    XboxController driverController = new XboxController(0);
+
+    // ドライブベースのデフォルトコマンドセット
+    swerve.setDefaultCommand(
+        swerve.teleopDrive(
+            // WPILibの数学は+X=前、+Y=左なんだけど、コントローラーのスティックは+X=右、+Y=下なの。
+            // あとスティックドリフトはありだからMathUtil.applyDeadbandで小さい入力は無視
+            () -> MathUtil.applyDeadband(-driverController.getLeftY(), .05),
+            () -> MathUtil.applyDeadband(-driverController.getLeftY(), .05),
+            () -> MathUtil.applyDeadband(-driverController.getLeftY(), .05)));
   }
 
-  /**
-   * This method is called every 20 ms, no matter the mode. Use this for items like diagnostics that
-   * you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic methods, but before LiveWindow and SmartDashboard
-   * integrated updating.
-   */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    // Runs the Scheduler. This is responsible for polling buttons, adding newly-scheduled commands,
+    // running already-scheduled commands, removing finished or interrupted commands, and running
+    // subsystem periodic() methods. This must be called from the robot's periodic block in order
+    // for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
+  private Command autoCommand;
+
   @Override
   public void autonomousInit() {
-    autoSelected = chooser.getSelected();
-    // autoSelected = SmartDashboard.getString("Auto Selector", DEFAULT_AUTO);
-    System.out.println("Auto selected: " + autoSelected);
+    autoCommand = chooser.getSelected();
+    autoCommand.schedule();
   }
 
-  /** This method is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
-    switch (autoSelected) {
-      case CUSTOM_AUTO:
-        // Put custom auto code here
-        break;
-      case DEFAULT_AUTO:
-      default:
-        // Put default auto code here
-        break;
-    }
+  public void autonomousExit() {
+    autoCommand.cancel();
   }
-
-  /** This method is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {}
-
-  /** This method is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {
-    // WPILibの数学は+X=前、+Y=左なんだけど、コントローラーのスティックは+X=右、+Y=下なの。
-    // あとスティックドリフトはありだからMathUtil.applyDeadbandで小さい入力は無視
-    // ドライバーの入力は+-1だからそれ x 最大速度 = ターゲットの速度
-    swerve.drive(
-        new ChassisSpeeds(
-            MathUtil.applyDeadband(-driverController.getLeftY(), .05)
-                * Swerve.maxLinearVelocityMetersPerSec,
-            MathUtil.applyDeadband(-driverController.getLeftX(), .05)
-                * Swerve.maxLinearVelocityMetersPerSec,
-            MathUtil.applyDeadband(-driverController.getRightX(), .05)
-                * Swerve.maxAngularVelocityRadiansPerSec));
-  }
-
-  /** This method is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
-
-  /** This method is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
 }
--- src/main/java/frc/robot/swerve/Swerve.java
+++ src/main/java/frc/robot/swerve/Swerve.java
@@ -4,19 +4,22 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
 
-public class Swerve {
+public class Swerve extends SubsystemBase {
   private final Module[] modules;
 
   private final SwerveDriveKinematics kinematics;
 
   // ホイールからホイールの幅、メートル
-  public static final double trackWidthMeters = .5;
-  public static final double trackLengthMeters = .5;
+  private final double trackWidthMeters = .5;
+  private final double trackLengthMeters = .5;
 
   // ドライブベースの最大速度。秒速メートルと秒速ラジアン
-  public static final double maxLinearVelocityMetersPerSec = Module.wheelMaxLinearVelocity;
-  public static final double maxAngularVelocityRadiansPerSec =
+  private final double maxLinearVelocityMetersPerSec = Module.wheelMaxLinearVelocity;
+  private final double maxAngularVelocityRadiansPerSec =
       Module.wheelMaxLinearVelocity / Math.hypot(trackLengthMeters / 2, trackWidthMeters / 2);
 
   public Swerve() {
@@ -31,7 +34,19 @@ public class Swerve {
             new Translation2d(-trackWidthMeters / 2, -trackLengthMeters / 2));
   }
 
-  public void drive(ChassisSpeeds desiredSpeeds) {
+  public Command teleopDrive(
+      DoubleSupplier xInput, DoubleSupplier yInput, DoubleSupplier yawInput) {
+    return run(
+        () ->
+            drive(
+                // ドライバーの入力は+-1だからそれ x 最大速度 = ターゲットの速度
+                new ChassisSpeeds(
+                    xInput.getAsDouble() * maxLinearVelocityMetersPerSec,
+                    yInput.getAsDouble() * maxLinearVelocityMetersPerSec,
+                    yawInput.getAsDouble() * maxAngularVelocityRadiansPerSec)));
+  }
+
+  private void drive(ChassisSpeeds desiredSpeeds) {
     // ターゲットの速度と角度を計算
     SwerveModuleState[] targetModuleSpeeds = kinematics.toSwerveModuleStates(desiredSpeeds);
     // ターゲットの速度があまりに速すぎないように、たまにはスピードを下げる
